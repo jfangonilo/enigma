993cf3b13bcf23843f82967171acec4bdefcb448
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-01 17:56:13 -0600

    Add test_it_exists

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -2,5 +2,11 @@ require_relative 'test_helper'
 require './lib/offset'
 
 class OffsetTest < MiniTest::Test
+  def setup
+    @offset = Offset.new
+  end
 
+  def test_it_exists
+    assert_instance_of Offset, @offset
+  end
 end
