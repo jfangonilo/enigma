c36e075b29504a9759db4fb137b759bbe458f7c0
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-01 17:37:07 -0600

    Add key generation tests.

--- test/key_test.rb
+++ test/key_test.rb
@@ -1,23 +1,9 @@
-require_relative './test_helper'
+require_relative 'test_helper'
 require './lib/key'
 
 class KeyTest < MiniTest::Test
   def setup
     @key = Key.new
-    @mock_key = mock("key")
-    @mock_key.number.returns(1234)
-    # @mock_key.number_as_string.returns("01234")
-    # 
-    #
-    # array_1 = ["0", "1", "2", "3", "4"]
-    # @mock_key.number_string_as_array.returns(array_1)
-    #
-    #
-    # array_2 = [["0", "1"],["1", "2"],["2", "3"],["3", "4"]]
-    # @mock_key.keys_step_one.returns(array_2)
-    #
-    # array_3 = [["01"],["12"],["23"],["34"]]
-    # @mock_key.keys_step_two.returns(array_3)
   end
 
   def test_it_exists
@@ -25,18 +11,37 @@ class KeyTest < MiniTest::Test
   end
 
   def test_it_gets_number
-    assert_equal 1234, @mock_key.number
+    assert_instance_of Integer, @key.number
   end
 
   def test_it_can_convert_to_string
-    assert_equal "01234", @mock_key.number_as_string
+    @key.stubs(:number_as_string).returns("01234")
+    assert_equal "01234", @key.number_as_string
   end
 
-  def test_keys_step_one
-    assert_equal ["0", "1", "2", "3", "4"], @mock_key.keys_step_one
+  def test_number_string_as_array
+    array = ["0", "1", "2", "3", "4"]
+    @key.stubs(:number_string_as_array).returns(array)
+    assert_equal array, @key.number_string_as_array
   end
 
-  def test_keys_step_two
-    assert_equal [["01"],["12"],["23"],["34"]], @mock_key.keys_step_two
+  def test_keys_hash
+    array = [["0", "1"], ["1", "2"],["2", "3"],["3", "4"]]
+    @key.stubs(:keys_array).returns(array)
+    assert_equal array, @key.keys_array
+  end
+
+  def test_letter_keys
+    @key.stubs(:a).returns(1)
+    assert_equal 1, @key.a
+
+    @key.stubs(:b).returns(12)
+    assert_equal 12, @key.b
+
+    @key.stubs(:c).returns(23)
+    assert_equal 23, @key.c
+
+    @key.stubs(:d).returns(34)
+    assert_equal 34, @key.d
   end
 end
