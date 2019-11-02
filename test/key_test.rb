f0a1bc9f69a0356554e44eb7bfbe36c099bcd7b9
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-02 16:30:42 -0600

    Move stubs to setup for key_test

--- test/key_test.rb
+++ test/key_test.rb
@@ -4,6 +4,7 @@ require './lib/key'
 class KeyTest < MiniTest::Test
   def setup
     @key = Key.new
+    @key.stubs(:number).returns("01234")
   end
 
   def test_it_exists
@@ -15,29 +16,20 @@ class KeyTest < MiniTest::Test
   end
 
   def test_number_string_as_array
-    @key.stubs(:number).returns("01234")
     array = ["0", "1", "2", "3", "4"]
     assert_equal array, @key.number_string_as_array
   end
 
   def test_keys_array
-    @key.stubs(:number).returns("01234")
     array = ["01", "12", "23", "34"]
     assert_equal array, @key.keys_array
   end
 
 
   def test_letter_keys
-    @key.stubs(:a).returns(1)
     assert_equal 1, @key.a
-
-    @key.stubs(:b).returns(12)
     assert_equal 12, @key.b
-
-    @key.stubs(:c).returns(23)
     assert_equal 23, @key.c
-
-    @key.stubs(:d).returns(34)
     assert_equal 34, @key.d
   end
 end
