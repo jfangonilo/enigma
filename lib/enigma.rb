7bf4b7fd6dc81593d3992fb2f9e8fb71c54e871c
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-03 08:47:01 -0700

    Add shift_for_key method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,17 @@
-require_relative 'shift'
+require_relative 'key'
+require_relative 'date'
 
 class Enigma
+  attr_reader :message, :key, :date
+
+  def initialize
+    @message = "hello world"
+    @key = Key.new
+    @date = EncodeDate.new
+  end
+
+  def shift_for_key
+    key.chars.each_cons(2).map {|number| number.join}.map {|key| key.to_i}
+  end
 
 end
