2684bb61384e636763891ab4a5b36d84574af06c
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-02 20:21:26 -0600

    Refactor offset class and tests

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -3,38 +3,16 @@ require './lib/offset'
 
 class OffsetTest < MiniTest::Test
   def setup
-    @offset = Offset.new
-    @offset.stubs(:date_string).returns("090788")
+    Offset.stubs(:date).returns("090788")
   end
 
-  def test_it_exists
-    assert_instance_of Offset, @offset
+  def test_offset_array
+    expected = ["0", "9", "4", "4"]
+    assert_equal expected, Offset.offset_array
   end
 
-  def test_it_can_get_date
-    assert_instance_of Time, @offset.date
-  end
-
-  def test_get_date_string
-    assert_instance_of String, @offset.date_string
-  end
-
-  def test_date_int
-    assert_equal 90_788, @offset.date_int
-  end
-
-  def test_date_squared
-    assert_equal 8_242_460_944, @offset.date_squared
-  end
-
-  def test_offset_digits
-    assert_equal ["0", "9", "4", "4"], @offset.digits_array
-  end
-
-  def test_letter_offsets
-    assert_equal 0, @offset.a
-    assert_equal 9, @offset.b
-    assert_equal 4, @offset.c
-    assert_equal 4, @offset.d
+  def test_offset_shifts
+    expected = [0, 9, 4, 4]
+    assert_equal expected, Offset.shifts
   end
 end
