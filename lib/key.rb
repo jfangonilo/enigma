abc510d0a0b70e946d2a606e15a885f98df2265d
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-02 20:09:28 -0600

    Refactor key class and tests

--- lib/key.rb
+++ lib/key.rb
@@ -1,33 +1,13 @@
 class Key
-  attr_reader :number
-
-  def initialize
-    @number = rand.to_s[2..6]
+  def self.random_number
+    rand.to_s[2..6].chars
   end
 
-  def number_string_as_array
-    number.chars
+  def self.keys_array
+    random_number.each_cons(2).map {|number| number.join}
   end
 
-  def keys_array
-    keys = []
-    number_string_as_array.each_cons(2) {|number| keys << number}
-    keys.map {|key| key.join}
-  end
-
-  def a
-    keys_array[0].to_i
-  end
-
-  def b
-    keys_array[1].to_i
-  end
-
-  def c
-    keys_array[2].to_i
-  end
-
-  def d
-    keys_array[3].to_i
+  def self.shifts
+    keys_array.map {|key| key.to_i}
   end
 end
--- test/key_test.rb
+++ test/key_test.rb
@@ -3,33 +3,15 @@ require './lib/key'
 
 class KeyTest < MiniTest::Test
   def setup
-    @key = Key.new
-    @key.stubs(:number).returns("01234")
-  end
-
-  def test_it_exists
-    assert_instance_of Key, @key
-  end
-
-  def test_it_gets_number
-    assert_instance_of String, @key.number
-  end
-
-  def test_number_string_as_array
-    array = ["0", "1", "2", "3", "4"]
-    assert_equal array, @key.number_string_as_array
+    Key.stubs(:random_number).returns(["0","1","2","3","4"])
   end
 
   def test_keys_array
-    array = ["01", "12", "23", "34"]
-    assert_equal array, @key.keys_array
+    expected = ["01", "12", "23", "34"]
+    assert_equal expected, Key.keys_array
   end
 
-
-  def test_letter_keys
-    assert_equal 1, @key.a
-    assert_equal 12, @key.b
-    assert_equal 23, @key.c
-    assert_equal 34, @key.d
+  def test_shift
+    assert_equal [1, 12, 23, 34], Key.shifts
   end
 end
