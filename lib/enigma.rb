77b5d810eca6410bded9c2b102b37dce4c4ca889
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-03 09:16:58 -0700

    Add shifts method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,12 +2,13 @@ require_relative 'key'
 require_relative 'date'
 
 class Enigma
-  attr_reader :message, :key, :date
+  attr_reader :message, :key, :date, :alphabet
 
   def initialize
-    @message = "hello world"
+    @message = ""
     @key = Key.new
     @date = EncodeDate.new
+    @alphabet = ("a".."z").to_a << " "
   end
 
   def shift_for_key
@@ -16,6 +17,11 @@ class Enigma
 
   def shift_for_date
     (date.to_i ** 2).to_s.chars.last(4).map {|offset| offset.to_i}
+  end
+
+  def shifts
+    shifts = [shift_for_key, shift_for_date]
+    shifts.transpose.map {|shift| shift.sum}
   end
 
   def chop_message
