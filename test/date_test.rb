fba46504685003ecd71a8ab7715a551f434ceae5
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-03 08:11:16 -0700

    Add test for date to initialize as string

--- test/date_test.rb
+++ test/date_test.rb
@@ -1,13 +1,18 @@
 require_relative 'test_helper'
 require './lib/date'
 
-class DateTest < MiniTest::Test
+class EncodeDateTest < MiniTest::Test
   def setup
-    @day = Date.new
+    @day = EncodeDate.new
+    @day.stubs(:date).returns("090788")
   end
 
   def test_it_exists
-    assert_instance_of Date, @day
+    assert_instance_of EncodeDate, @day
   end
 
+  def test_it_has_date_as_string
+    assert_equal "090788", @day.date
+    assert_instance_of String, @day.date
+  end
 end
