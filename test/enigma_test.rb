f0c555757308a661c80b440a0cf910f2ca17159b
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-03 07:03:31 -0700

    Add shift require and shift stubs

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,12 +1,16 @@
 require_relative 'test_helper'
 require './lib/enigma'
+require './lib/shift'
 
 class EnigmaTest < MiniTest::Test
   def setup
-    @enigma = Enigma.new
+    Shift.stubs(:a).returns(1)
+    Shift.stubs(:b).returns(21)
+    Shift.stubs(:c).returns(27)
+    Shift.stubs(:d).returns(38)
   end
 
   def test_it_exists
-    assert_instance_of Enigma, @enigma
+    assert_instance_of Enigma, Enigma.new
   end
 end
