b60e3d7ff73ca2974a8c2442c8122332be2ecf1b
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-02 20:35:37 -0600

    Add methods for Shift

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,3 +1,20 @@
-class Shift
+require_relative 'key'
+require_relative 'offset'
 
+class Shift
+  def self.a
+    Key.shifts[0] + Offset.shifts[0]
+  end
+
+  def self.b
+    Key.shifts[1] + Offset.shifts[1]
+  end
+
+  def self.c
+    Key.shifts[2] + Offset.shifts[2]
+  end
+
+  def self.d
+    Key.shifts[3] + Offset.shifts[3]
+  end
 end
