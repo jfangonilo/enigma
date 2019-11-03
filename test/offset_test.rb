9651c34a7cebb47994bfa9595de720f4ea4a5899
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-03 07:25:43 -0700

    Rewrite offset to use instance variables

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -3,33 +3,34 @@ require './lib/offset'
 
 class OffsetTest < MiniTest::Test
   def setup
-    Offset.stubs(:date).returns("090788")
+    @offset = Offset.new
+    @offset.stubs(:date).returns("090788")
   end
 
   def test_it_exists
-    assert_instance_of Offset, Offset.new
+    assert_instance_of Offset, @offset
   end
 
   def test_date_squared
-    assert_equal 8_242_460_944, Offset.date_squared
+    assert_equal 8_242_460_944, @offset.date_squared
   end
 
   def test_date_squared_to_str
-    assert_equal "8242460944", Offset.date_sqrd_str
+    assert_equal "8242460944", @offset.date_sqrd_str
   end
 
   def test_date_sqrd_array
     expected = ["8", "2", "4", "2", "4", "6", "0", "9", "4", "4"]
-    assert_equal expected, Offset.date_sqrd_array
+    assert_equal expected, @offset.date_sqrd_array
   end
 
   def test_offset_array
     expected = ["0", "9", "4", "4"]
-    assert_equal expected, Offset.offset_array
+    assert_equal expected, @offset.offset_array
   end
 
   def test_offset_shifts
     expected = [0, 9, 4, 4]
-    assert_equal expected, Offset.shifts
+    assert_equal expected, @offset.shifts
   end
 end
