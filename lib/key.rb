1124aef55be4be5df55c201bde89029f88bed7c9
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-03 19:15:48 -0700

    Add key validation methods

--- lib/key.rb
+++ lib/key.rb
@@ -1,7 +1,20 @@
 class Key
   attr_reader :number
 
-  def initialize
-    @number = rand.to_s[2..6]
+  def initialize(number = nil)
+    @number = generate_key(number)
   end
+
+  def generate_key(number)
+    if number.nil? || number.count("0123456789") != 5
+      random_number
+    else
+      number
+    end
+  end
+
+  def random_number
+    rand.to_s[2..6]
+  end
+
 end
