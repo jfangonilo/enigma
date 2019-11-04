be5ae31e4e1d6e1f5a434255a006179d70ece994
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-04 12:53:13 -0700

    Rewrite shift tests for shift class rework

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -2,27 +2,25 @@ require_relative 'test_helper'
 require './lib/shift'
 
 class ShiftTest < MiniTest::Test
-  # def setup
-  #   @shift = Shift.new
-  # end
-  #
-  # def test_it_exists
-  #   assert_instance_of Shift, @shift
-  # end
-  #
-  # def test_a_shift
-  #   assert_instance_of Integer, @shift.a
-  # end
-  #
-  # def test_b_shift
-  #   assert_instance_of Integer, @shift.b
-  # end
-  #
-  # def test_c_shift
-  #   assert_instance_of Integer, @shift.c
-  # end
-  #
-  # def test_d_shift
-  #   assert_instance_of Integer, @shift.d
-  # end
+  def setup
+    @shift = Shift.new
+  end
+
+  def test_it_exists
+    assert_instance_of Shift, @shift
+  end
+
+  def test_key_shift
+    assert_equal [1, 12, 23, 34], Shift.key_shift("01234")
+  end
+
+  def test_date_shift
+    assert_equal [0, 9, 4, 4], Shift.date_shift("090788")
+  end
+
+  def test_rotation
+    key = "01234"
+    date = "090788"
+    assert_equal [1, 21, 27, 38], Shift.final(key, date)
+  end
 end
