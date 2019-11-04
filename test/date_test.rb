6f32efcdd6205e2cdf8969fad8d7c30a10ed18cb
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-03 19:30:00 -0700

    Add date validation tests

--- test/date_test.rb
+++ test/date_test.rb
@@ -14,4 +14,17 @@ class EncodeDateTest < MiniTest::Test
   def test_it_has_date_as_string
     assert_equal "090788", @day.date
   end
+
+  def test_valid_date
+    date = "031119"
+    assert_equal "031119", @day.valid_date(date)
+    date = "010120"
+    assert_equal "010120", @day.valid_date(date)
+    date = "aflkha"
+    refute_equal "aflkha", @day.valid_date(date)
+    date = "0121"
+    refute_equal "0121", @day.valid_date(date)
+    date = "1414111"
+    refute_equal "1414111", @day.valid_date(date)
+  end
 end
