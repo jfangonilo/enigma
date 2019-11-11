06675cf5217ba2e170f2c6a983eaefcc968d5814
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-10 21:13:28 -0700

    Test cleanup

--- test/alphabet_test.rb
+++ test/alphabet_test.rb
@@ -2,14 +2,6 @@ require_relative 'test_helper'
 require './lib/alphabet'
 
 class AlphabetTest < MiniTest::Test
-  def setup
-    @alphabet = Alphabet.new
-  end
-
-  def test_it_exists
-    assert_instance_of Alphabet, @alphabet
-  end
-
   def test_it_has_string_of_alphabet
     assert_equal "abcdefghijklmnopqrstuvwxyz ", Alphabet.default
   end
--- test/key_test.rb
+++ test/key_test.rb
@@ -2,14 +2,6 @@ require_relative 'test_helper'
 require './lib/key'
 
 class KeyTest < MiniTest::Test
-  def setup
-    @key = Key.new
-  end
-
-  def test_it_exists
-    assert_instance_of Key, @key
-  end
-
   def test_it_creates_5_digit_number_as_string
     assert Key.number.count("0123456789") == 5
   end
